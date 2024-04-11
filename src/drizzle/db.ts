0b7b1c76bfdc491379eb17d5f6abecf20945c217
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-04-11 16:16:33 +0100

    Refactor database client creation
    Moved the creation of the Postgres client from a separate module to directly within the main database file. This simplifies the code structure and reduces unnecessary function calls. The environment variable check for 'POSTGRES_URL' is now implicitly handled by the postgres library itself.

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "./schema";
-import { createClient } from "@/drizzle/client";
 
-export const client = createClient();
+// Disable prefetch as it is not supported for "Transaction" pool mode
+export const client = postgres(process.env.POSTGRES_URL as string, {
+  prepare: false,
+});
 export const db = drizzle(client, { schema, logger: true });
