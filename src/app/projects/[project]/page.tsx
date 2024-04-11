d1e5ded2e9b36bdc0e4c4c780f64100fb440f7fa
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-04-11 14:11:20 +0100

    Add project page with authentication
    A new project page has been added. This page fetches a specific project based on the UUID passed as a parameter. It also includes user authentication, redirecting to the login page if no user is found or if there's an error fetching the user data. The fetched project is displayed only if the logged-in user matches the owner of the project, otherwise, an access denial message is shown.

---
+++ src/app/projects/[project]/page.tsx
@@ -0,0 +1,40 @@
+import { UUID } from "crypto";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+type ProjectProps = {
+  params: { project: UUID };
+};
+
+export default async function Project({ params }: ProjectProps) {
+  const supabase = createClient();
+  const { data: user, error: userError } = await supabase.auth.getUser();
+
+  if (userError || !user?.user) {
+    redirect("/login");
+  }
+
+  const { data: fetchedProject, error: fetchedProjectError } = await supabase
+    .from("projects")
+    .select("*")
+    .eq("id", params.project)
+    .single();
+
+  if (fetchedProjectError) {
+    console.log(fetchedProjectError);
+    return <p>Failed to fetch project</p>;
+  }
+
+  if (user?.user.email !== fetchedProject?.user) {
+    return <p>You do not have access to this project</p>;
+  }
+
+  console.log(fetchedProject);
+
+  return (
+    <>
+      <h1>{fetchedProject.project_name}</h1>
+      <p>{fetchedProject.project_short_description}</p>
+    </>
+  );
+}
