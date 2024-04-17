4533f9fe290033e424d9d9fabf0a645dac491158
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-04-18 00:28:37 +0100

    Reduce copy status timeout duration
    The timeout duration for the reference copy status has been halved. This change will make the "copied" message disappear faster, improving user experience.

--- src/components/estimateRequestForm/EstimateRequestForm.tsx
+++ src/components/estimateRequestForm/EstimateRequestForm.tsx
@@ -57,7 +57,7 @@ export function EstimateRequestForm() {
     setRefCopyStatus("copied");
     setTimeout(() => {
       setRefCopyStatus("");
-    }, 2000);
+    }, 1000);
   };
 
   const handleHover = () => {
