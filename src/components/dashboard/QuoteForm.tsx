7e338e7878f66be85e862142f186479b065158e2
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-04-24 12:28:11 +0100

    Refactor QuoteForm and update CSS
    The QuoteForm component has been refactored to improve readability and maintainability. The changes include:
    - Replacing the previous nested conditional rendering with a more structured approach using map functions.
    - Adjusting the visibility toggling logic to accommodate the new structure.
    - Renaming variables for clarity.
    
    In addition, updates have been made to 'globals.css' to introduce new styles for '.quote-ul ul' and its child elements. These changes enhance the visual presentation of list items in the QuoteForm component.

--- src/components/dashboard/QuoteForm.tsx
+++ src/components/dashboard/QuoteForm.tsx
@@ -25,84 +25,92 @@ export default function QuoteForm({
   };
 
   return (
-    <ul className="mt-6 text-left">
-      {level1.map((nrm) => (
-        <li key={nrm.id} className="pt-2">
+    <ul className="quote-ul mt-6 text-left">
+      {level1.map((nrm1) => (
+        <li key={nrm1.id} className="mb-2">
           <div className="flex">
             <Switch
               className="mr-2 mt-1"
-              checked={!!visible[`${nrm.flag_1}`]}
-              onCheckedChange={() => toggleVisibility(`${nrm.flag_1}`)}
+              checked={!!visible[`${nrm1.flag_1}`]}
+              onCheckedChange={() => toggleVisibility(`${nrm1.flag_1}`)}
             />
-            <p>{nrm.el_1}</p>
+            <p>{nrm1.el_1}</p>
           </div>
-          {visible[`${nrm.flag_1}`] && (
-            <ul className="ml-8 pt-2">
+          {visible[`${nrm1.flag_1}`] && (
+            <ul className="mb-2 ml-8">
               {level2.map(
-                (nrm) =>
-                  nrm.flag_2 !== 0 && (
-                    <li key={nrm.id}>
+                (nrm2) =>
+                  nrm2.flag_2 !== 0 &&
+                  nrm2.flag_1 === nrm1.flag_1 && (
+                    <li key={nrm2.id}>
                       <div className="flex">
                         <Switch
                           className="mr-2 mt-1"
-                          checked={!!visible[`${nrm.flag_1}${nrm.flag_2}`]}
+                          checked={!!visible[`${nrm2.flag_1}${nrm2.flag_2}`]}
                           onCheckedChange={() =>
-                            toggleVisibility(`${nrm.flag_1}${nrm.flag_2}`)
+                            toggleVisibility(`${nrm2.flag_1}${nrm2.flag_2}`)
                           }
                         />
-                        <p>{nrm.el_2}</p>
+                        <p>{nrm2.el_2}</p>
                       </div>
-                      {visible[`${nrm.flag_1}${nrm.flag_2}`] && (
-                        <ul className="ml-8 pt-2">
+                      {visible[`${nrm2.flag_1}${nrm2.flag_2}`] && (
+                        <ul className="mb-2 ml-8">
                           {level3.map(
-                            (nrm) =>
-                              nrm.flag_3 !== 0 && (
-                                <li key={nrm.id}>
+                            (nrm3) =>
+                              nrm3.flag_3 !== 0 &&
+                              nrm3.flag_2 === nrm2.flag_2 &&
+                              nrm3.flag_1 === nrm2.flag_1 && (
+                                <li key={nrm3.id}>
                                   <div className="flex">
                                     <Switch
                                       className="mr-2 mt-1"
                                       checked={
                                         !!visible[
-                                          `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}`
+                                          `${nrm3.flag_1}${nrm3.flag_2}${nrm3.flag_3}`
                                         ]
                                       }
                                       onCheckedChange={() =>
                                         toggleVisibility(
-                                          `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}`,
+                                          `${nrm3.flag_1}${nrm3.flag_2}${nrm3.flag_3}`,
                                         )
                                       }
                                     />
-                                    <p>{nrm.el_3}</p>
+                                    <p>{nrm3.el_3}</p>
                                   </div>
                                   {visible[
-                                    `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}`
+                                    `${nrm3.flag_1}${nrm3.flag_2}${nrm3.flag_3}`
                                   ] && (
-                                    <ul className="ml-8 pt-2">
-                                      {level4.map((nrm) => (
-                                        <li key={nrm.id}>
-                                          <div className="flex">
-                                            <Switch
-                                              className="mr-2 mt-1"
-                                              checked={
-                                                !!visible[
-                                                  `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}${nrm.flag_4}`
-                                                ]
-                                              }
-                                              onCheckedChange={() =>
-                                                toggleVisibility(
-                                                  `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}${nrm.flag_4}`,
-                                                )
-                                              }
-                                            />
-                                            <div>
-                                              <p>{nrm.el_4}</p>
-                                              {visible[
-                                                `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}${nrm.flag_4}`
-                                              ] && <p>CALCULATIONS</p>}
-                                            </div>
-                                          </div>
-                                        </li>
-                                      ))}
+                                    <ul className="mb-2 ml-8">
+                                      {level4.map(
+                                        (nrm4) =>
+                                          nrm4.flag_3 === nrm3.flag_3 &&
+                                          nrm4.flag_2 === nrm3.flag_2 &&
+                                          nrm4.flag_1 === nrm3.flag_1 && (
+                                            <li key={nrm4.id}>
+                                              <div className="flex">
+                                                <Switch
+                                                  className="mr-2 mt-1"
+                                                  checked={
+                                                    !!visible[
+                                                      `${nrm4.flag_1}${nrm4.flag_2}${nrm4.flag_3}${nrm4.flag_4}`
+                                                    ]
+                                                  }
+                                                  onCheckedChange={() =>
+                                                    toggleVisibility(
+                                                      `${nrm4.flag_1}${nrm4.flag_2}${nrm4.flag_3}${nrm4.flag_4}`,
+                                                    )
+                                                  }
+                                                />
+                                                <div>
+                                                  <p>{nrm4.el_4}</p>
+                                                  {visible[
+                                                    `${nrm4.flag_1}${nrm4.flag_2}${nrm4.flag_3}${nrm4.flag_4}`
+                                                  ] && <p>CALCULATIONS</p>}
+                                                </div>
+                                              </div>
+                                            </li>
+                                          ),
+                                      )}
                                     </ul>
                                   )}
                                 </li>
@@ -120,3 +128,77 @@ export default function QuoteForm({
     </ul>
   );
 }
+
+//   return (
+//     <div className="mt-6 text-left">
+//       {nrmData?.map((nrm) =>
+//         nrm.flag_2 === 0 ? (
+//           <div key={nrm.id} className="flex pt-2">
+//             <Switch
+//               className="mr-2 mt-1"
+//               checked={!!visible[`${nrm.flag_1}`]}
+//               onCheckedChange={() => toggleVisibility(`${nrm.flag_1}`)}
+//             />
+//             <p>{nrm.el_1}</p>
+//           </div>
+//         ) : nrm.flag_3 === 0 ? (
+//           visible[`${nrm.flag_1}`] && (
+//             <div key={nrm.id} className="ml-8 flex pt-2">
+//               <Switch
+//                 className="mr-2 mt-1"
+//                 checked={!!visible[`${nrm.flag_1}${nrm.flag_2}`]}
+//                 onCheckedChange={() =>
+//                   toggleVisibility(`${nrm.flag_1}${nrm.flag_2}`)
+//                 }
+//               />
+//               <p>{nrm.el_2}</p>
+//             </div>
+//           )
+//         ) : nrm.flag_4 === 0 ? (
+//           visible[`${nrm.flag_1}`] &&
+//           visible[`${nrm.flag_1}${nrm.flag_2}`] && (
+//             <div key={nrm.id} className="ml-16 flex pt-2">
+//               <Switch
+//                 className="mr-2 mt-1"
+//                 checked={!!visible[`${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}`]}
+//                 onCheckedChange={() =>
+//                   toggleVisibility(`${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}`)
+//                 }
+//               />
+//               <p>{nrm.el_3}</p>
+//             </div>
+//           )
+//         ) : (
+//           visible[`${nrm.flag_1}`] &&
+//           visible[`${nrm.flag_1}${nrm.flag_2}`] &&
+//           visible[`${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}`] && (
+//             <div key={nrm.id} className="ml-24 flex pt-2">
+//               <Switch
+//                 className="mr-2 mt-1"
+//                 checked={
+//                   !!visible[
+//                     `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}${nrm.flag_4}`
+//                   ]
+//                 }
+//                 onCheckedChange={() =>
+//                   toggleVisibility(
+//                     `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}${nrm.flag_4}`,
+//                   )
+//                 }
+//               />
+//               <div className="flex flex-col">
+//                 <p>{nrm.el_4}</p>
+//                 {visible[`${nrm.flag_1}`] &&
+//                   visible[`${nrm.flag_1}${nrm.flag_2}`] &&
+//                   visible[`${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}`] &&
+//                   visible[
+//                     `${nrm.flag_1}${nrm.flag_2}${nrm.flag_3}${nrm.flag_4}`
+//                   ] && <p>CALCULATIONS</p>}
+//               </div>
+//             </div>
+//           )
+//         ),
+//       )}
+//     </div>
+//   );
+// }
