a2d60bdda8236be28838bdd41049f5cdd4346bff
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-09 08:52:02 +0100

    Refactor contractor list fetching logic
    - Remove unnecessary console logs from the contractor mapping function
    - Change filtering criteria in array to use userId instead of costId for both frontItems and remainingItems

--- src/utils/getContractorsComboboxList.ts
+++ src/utils/getContractorsComboboxList.ts
@@ -17,10 +17,7 @@ export async function getContractorsComboboxList({
   contractors,
   supabaseClient,
 }: getContractorsComboboxListProps) {
-  contractors.map((contractor) => {
-    console.log(`contractor.contractor_costs.id`);
-    console.log(contractor.contractor_costs.id);
-  });
+  contractors.map((contractor) => {});
   let array = contractors.map((contractor) => ({
     label: contractor.name,
     value: contractor.name,
@@ -30,9 +27,9 @@ export async function getContractorsComboboxList({
 
   const admins = await fetchAdmins(supabaseClient);
   const adminIds = admins.map((admin) => admin.id);
-  const frontItems = array.filter((item) => adminIds.includes(item.costId));
+  const frontItems = array.filter((item) => adminIds.includes(item.userId));
   const remainingItems = array.filter(
-    (item) => !adminIds.includes(item.costId),
+    (item) => !adminIds.includes(item.userId),
   );
   const newArray = frontItems.concat(remainingItems);
 
