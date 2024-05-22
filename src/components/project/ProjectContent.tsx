0bd2052414fe34ab43784dc8b2dff02d87ef6c04
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-22 12:17:31 +0100

    Update flexbox alignment in project content
    - Adjusted the CSS class to improve item alignment within the flex container. This change enhances layout consistency across different screen sizes.

--- src/components/project/ProjectContent.tsx
+++ src/components/project/ProjectContent.tsx
@@ -145,7 +145,7 @@ export default async function projectContent({
       <section
       // className="todo"
       >
-        <div className="flex items-center gap-12">
+        <div className="flex items-center justify-between gap-12">
           <h2 className="text-2xl">Quotes</h2>
           <Button asChild>
             <Link href={`/dashboard/quotes/new/${fetchedProject.id}`}>
