ddee6041f74307107dba3ca60f970aefc7ce3144
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-25 13:00:21 +0100

    Enhance Rates page layout
    Improve the layout of the Rates page in the admin dashboard. Add a DashboardMenu component and adjust container styling for better visual presentation. Also, include a header to improve user navigation.

--- src/app/admin/dashboard/rates/page.tsx
+++ src/app/admin/dashboard/rates/page.tsx
@@ -1,3 +1,13 @@
+import DashboardMenu from "@/components/dashboard/DashboardMenu";
+
 export default function Rates() {
-  return <div>Rates</div>;
+  return (
+    <div>
+      <h1 className="mb-6 text-2xl">Dashboard</h1>
+      <DashboardMenu defaultValue="rates" />
+      <div className="container mx-auto min-w-[50rem] py-4">
+        <p>RATES</p>
+      </div>
+    </div>
+  );
 }
