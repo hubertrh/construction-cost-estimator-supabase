0174ba8c291ad9382804379d6abc3906cfe51f9f
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-25 10:42:30 +0100

    Refactor user data fetching in new estimate request
    Simplified the process of fetching user data in the new estimate request function. Removed error handling related to this operation, as it was deemed unnecessary.

--- src/app/new-estimate-request/page.tsx
+++ src/app/new-estimate-request/page.tsx
@@ -3,11 +3,7 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function newEstimateRequest() {
   const supabase = createClient();
-  const { data: user, error: userError } = await supabase.auth.getUser();
-
-  if (userError && userError?.status !== 403) {
-    throw new Error("Failed to fetch user");
-  }
+  const { data: user } = await supabase.auth.getUser();
 
   return <EstimateRequestForm user={user?.user ? user.user : null} />;
 }
